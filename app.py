8fcfcac50e599cf9d60c09843762b946f8d40618
william baker <68308394+strayon3@users.noreply.github.com>
2025-01-25 10:56:23 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 import json
+import os
 from datetime import datetime
 from info import key
 
@@ -127,4 +128,5 @@ def search_recipes():
     return render_template("search.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0",port=port,debug=True)
